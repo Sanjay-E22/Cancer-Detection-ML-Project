82e72fc117587745bac8a5963f1a483e5279b176
Sanjay-E22 <sanjayelangovan22@gmail.com>
2025-07-18 11:42:58 +0530

    j

--- app.py
+++ app.py
@@ -16,12 +16,36 @@ st.write("Enter cell features below:")
 
 # User Inputs (first 10 features)
 features = []
-feature_names = [
-    "mean radius", "mean texture", "mean perimeter",
-    "mean area", "mean smoothness", "mean compactness",
-    "mean concavity", "mean concave points",
-    "mean symmetry", "mean fractal dimension"
-]
+feature_names = ['radius_mean',
+ 'texture_mean',
+ 'perimeter_mean',
+ 'area_mean',
+ 'smoothness_mean',
+ 'compactness_mean',
+ 'concavity_mean',
+ 'concave points_mean',
+ 'symmetry_mean',
+ 'fractal_dimension_mean',
+ 'radius_se',
+ 'texture_se',
+ 'perimeter_se',
+ 'area_se',
+ 'smoothness_se',
+ 'compactness_se',
+ 'concavity_se',
+ 'concave points_se',
+ 'symmetry_se',
+ 'fractal_dimension_se',
+ 'radius_worst',
+ 'texture_worst',
+ 'perimeter_worst',
+ 'area_worst',
+ 'smoothness_worst',
+ 'compactness_worst',
+ 'concavity_worst',
+ 'concave points_worst',
+ 'symmetry_worst',
+ 'fractal_dimension_worst']
 
 for name in feature_names:
     val = st.slider(name, 0.0, 100.0, 1.0)
